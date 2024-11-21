a38c890ba2066d035addeced554fb3dff8fba322
SmidgeonE <finlaynugent@gmail.com>
2024-11-21 15:22:21 +0000

    Added 2 new job cases

---
+++ bc4/boid_num_2000.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# =================
+# script.sh
+# =================
+
+#SBATCH --job-name=boid_num_500
+#SBATCH --partition=teach_cpu
+#SBATCH --account=PHYS033184
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=3:00:00
+#SBATCH --mem=2000M
+# Load modules required for runtime e.g.
+
+module load languages/Intel-OneAPI/2024.0.2
+
+cd $SLURM_SUBMIT_DIR
+export OMP_NUM_THREADS=1
+
+
+# Now run your program with the usual command
+
+srun --mpi=pmi2 /user/home/oz21652/Comp401/bc4/bin/2000.exe
+
